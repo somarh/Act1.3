b8c55464ee2b39e6530bfd0664ba6846bd6699cb
AdrianM0Hdz <morthem@LAPTOP-BAVC5R5S.localdomain>
2022-09-14 16:28:14 -0500

    added Event.hpp

--- include/Event.hpp
+++ include/Event.hpp
@@ -1,4 +1,25 @@
+#pragma once
+
+#include <string>
+
 class Event {
     private:
+        std::string message;
+        std::string date;
 
+    public:
+        Event() = default;
+        Event(std::string, std::string);
+
+        std::string getMessage() const;
+        std::string getDate() const;
+
+        void setMessage(std::string);
+        void setDate(std::string);
+
+        double getNumDate() const;
+
+        void loadData(std::string);
+
+        std::string str() const;
 };
